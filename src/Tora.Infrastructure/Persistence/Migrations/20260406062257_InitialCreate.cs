36659ad1806ce96da19c13e163cbbf0cfca8fc3a
Apurv Kumar <sapurv.skumar250@gmail.com>
2026-04-06 12:06:50 +0530

    refactor: change location of Migrations and databse (#1)

--- src/Tora.Infrastructure/Persistence/Migrations/20260406062257_InitialCreate.cs
+++ src/Tora.Infrastructure/Persistence/Migrations/20260406062257_InitialCreate.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace Tora.Infrastructure.Migrations
+namespace Tora.Infrastructure.Persistence.Migrations
 {
     /// <inheritdoc />
     public partial class InitialCreate : Migration
