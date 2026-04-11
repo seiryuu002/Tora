05ea024b27d7a2f4c7b63ee0a92423e3bf512afb
Apurv Kumar <sapurv.skumar250@gmail.com>
2026-04-11 23:39:32 +0530

    feat: add additional DbSet according to DB

--- src/Tora.Application/Interfaces/IToraDbContext.cs
+++ src/Tora.Application/Interfaces/IToraDbContext.cs
@@ -8,6 +8,9 @@ public interface IToraDbContext
     DbSet<User> Users {get;}
     DbSet<Role> Roles {get;}
     DbSet<RefreshToken> RefreshTokens {get;}
+    DbSet<Project> Projects {get;}
+    DbSet<Domain.Entities.Task> Tasks{get; }
+    DbSet<Comment> Comments { get; }
     Task<int> SaveChangesAsync(CancellationToken cancellationToken);
 
 }
